ae899812b5ecf4cac12f689c7e60096bb83c3d84
Rushikesh-ganesh <42743689+Rushikesh-ganesh@users.noreply.github.com>
2018-08-28 13:08:57 +0530

    Add files via upload

---
+++ ass.js
@@ -0,0 +1,37 @@
+// const months=['jan','feb','mar','apr','may','jun','july','aug','sup','oct','nov','dec']
+// //console.log(months[0]);
+// /*months.forEach(function(months,index){
+//     console.log(`the month is ${index+1}${months} `)
+// })*/
+// //this larger code  we print here jan dec
+// //for (let i = 0; i < months.length; i++) {
+//    // const element = months[i];
+//    // console.log(element)
+//     //console.log(months[index]);
+    
+// //}
+//  //at this time we print the dec to jan
+// for (let index =months.length-1;index>=0;index--){
+ // console.log(months[index]);
+// }
+
+const myTodos =[]
+myTodos.push(`buy bread`)
+myTodos.push(`learn coding`)
+myTodos.push(`go to interviews `)
+
+//myTodos.forEach(function(todos, index){
+  //  console.log(`your task no ${index+1}is:${todos}`);
+    
+//})
+
+//this is code normal print up to bottom
+for (let index = 0; index < myTodos.length; index++) {
+    const element = myTodos[index];
+    console.log(element);
+    
+    }
+
+    
+    
+
